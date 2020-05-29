9443e7abc04c16eae79c95b55187065a511ef6e0
osmar-asus <osmarfilho6@gmail.com>
2020-05-28 21:23:12 -0300

    (fix) Formulario de departamento

--- src/app/components/admin/departamento/departamento.module.ts
+++ src/app/components/admin/departamento/departamento.module.ts
@@ -4,12 +4,14 @@ import { ComumModule } from 'src/app/modules/comum/comum.module';
 
 import { DepartamentoComponent } from './departamento.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { DepartamentoRoutingModule } from './departamento-routing.module';
 
 @NgModule({
   declarations: [DepartamentoComponent],
   imports: [
     ComumModule,
     ReactiveFormsModule,
+    DepartamentoRoutingModule
   ]
 })
 export class DepartamentoModule { }
